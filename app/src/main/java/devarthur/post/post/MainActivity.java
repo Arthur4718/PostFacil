87b68941c9f07ca27ec4d17c2b1f0937bcb91960
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-17 16:06:37 -0300

    Open gallery button
    Added a new buton that checks if the user can open the gallery.

--- app/src/main/java/devarthur/post/post/MainActivity.java
+++ app/src/main/java/devarthur/post/post/MainActivity.java
@@ -5,20 +5,39 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
+import devarthur.post.post.Gallery.ProductsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private CallbackManager callbackManager;
+    private LoginButton loginButton;
+    private Button openGalleryButton;
+    private AccessToken accessToken;
+    private boolean isLoggedIn;
+    private static final String EMAIL = "email";
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
+        accessToken = AccessToken.getCurrentAccessToken();
+        isLoggedIn = accessToken != null && !accessToken.isExpired();
+        Log.d("User Logged", "" + String.valueOf(isLoggedIn));
+
+
         super.onActivityResult(requestCode, resultCode, data);
     }
 
@@ -28,26 +47,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
         callbackManager = CallbackManager.Factory.create();
 
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        Log.d("App","On Sucess: " + loginResult.toString());
-                    }
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
 
-                    @Override
-                    public void onCancel() {
-                        Log.d("App", "User canceled login");
-                    }
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("App", "On Success: " + loginResult.toString());
 
-                    @Override
-                    public void onError(FacebookException exception) {
-                        Log.d("App", "On Failure: " + exception.toString());
-                    }
-                });
 
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.d("App", "On Error: " + exception.toString());
+            }
+        });
+
+        Button openGalleryButton = (Button) findViewById(R.id.btnIniciar);
+        openGalleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isLoggedIn){
+                    Intent gallery = new Intent(getApplicationContext(), ProductsActivity.class);
+                    startActivity(gallery);
+                    finish();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Por favor efetue login", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
 
     }
 }
