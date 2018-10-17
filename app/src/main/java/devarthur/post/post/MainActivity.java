7d4f2849b74868d6584a99ec88dbf999bfb72158
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-17 14:33:30 -0300

    Gallery Activity layout
    Added a new Nav Drawer Layout.

--- app/src/main/java/devarthur/post/post/MainActivity.java
+++ app/src/main/java/devarthur/post/post/MainActivity.java
@@ -1,13 +1,53 @@
 package devarthur.post.post;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Log.d("App","On Sucess: " + loginResult.toString());
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.d("App", "User canceled login");
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Log.d("App", "On Failure: " + exception.toString());
+                    }
+                });
+
+
     }
 }
