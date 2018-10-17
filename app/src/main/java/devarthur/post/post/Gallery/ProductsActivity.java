f5e152b0db69994899d1b40cae6eaf6faf34c3fc
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-17 16:59:00 -0300

    Drawer Header
    Removed unused drawers from the folder. Removed header.

--- app/src/main/java/devarthur/post/post/Gallery/ProductsActivity.java
+++ app/src/main/java/devarthur/post/post/Gallery/ProductsActivity.java
@@ -26,15 +26,7 @@ public class ProductsActivity extends AppCompatActivity
         setContentView(R.layout.activity_products);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        toolbar.setTitle("Post Fácil");
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
